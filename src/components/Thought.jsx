93091198fe0acecc482136bd22d2f94753b522ed
Kris Albert Lee <krisalbert@gmail.com>
2023-10-31 20:54:13 +0100

    update styling

--- src/components/Thought.jsx
+++ src/components/Thought.jsx
@@ -15,17 +15,19 @@ export const Thought = ({
   return (
     <div className="thought">
       <div className="thought-message">{message}</div>
-      <div className="thought-hearts">
-        <Heart
-          _id={_id}
-          hearts={hearts}
-          handleFetchData={handleFetchData}
-          hearted={hearted}
-          setHearted={setHearted}
-        />
-      </div>
-      <div className="thought-time">
-        <ReactTimeAgo date={new Date(createdAt)} locale="en-US" />
+      <div className="thought-heart-time-wrapper">
+        <div className="thought-hearts">
+          <Heart
+            _id={_id}
+            hearts={hearts}
+            handleFetchData={handleFetchData}
+            hearted={hearted}
+            setHearted={setHearted}
+          />
+        </div>
+        <div className="thought-time">
+          <ReactTimeAgo date={new Date(createdAt)} locale="en-US" />
+        </div>
       </div>
     </div>
   );
